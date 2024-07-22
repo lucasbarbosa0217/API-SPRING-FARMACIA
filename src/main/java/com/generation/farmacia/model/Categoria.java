97ac705106151bb71326656d6110147bd1f3bcd4
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-07-22 10:00:23 -0300

    Correção Size

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Categoria {
@@ -14,6 +15,7 @@ public class Categoria {
 	private Long id;
 	
 	@NotBlank(message = "O campo descrição da categoria é obrigatório!")
+	@Size(min = 2, max = 30, message="O campo descrição deve ter entre 2 e 30 caracteres!")
 	private String descricao;
 
 	public Long getId() {
